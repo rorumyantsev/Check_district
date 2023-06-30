8c3a9c8210a28de8f65ca87ff94e0277460db0f6
rorumyantsev <126847670+rorumyantsev@users.noreply.github.com>
2023-06-30 13:22:20 -0500

    Update wh_report_per.py

--- wh_report_per.py
+++ wh_report_per.py
@@ -63,7 +63,7 @@ def get_geofix_report():
     except Exception as e:
         st.write(e)
     filtered_report_df = geofix_report_df[~geofix_report_df["Internal Status"].isin(["delivered"])]
-    filtered_report_df = filtered_report_df[geofix_report_df["Log platform latitude"] is not None]
+    filtered_report_df = filtered_report_df[~filtered_report_df["Log platform latitude"].isin([""])]
     st.write(filtered_report_df)
     return geofix_report_df
 
